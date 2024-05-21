d1e315ad102b1dbef66f9b9c754167f6208c3eec
AbdullohDev07 <gamesupro01@gmail.com>
2024-05-21 10:35:46 +0500

    add files

---
+++ src/app/Pages/novosti/novosti-one/novosti-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novosti-one',
+  templateUrl: './novosti-one.component.html',
+  styleUrl: './novosti-one.component.scss'
+})
+export class NovostiOneComponent {
+
+}
